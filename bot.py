434f9fbe9529535ca30352445176a042df3bf8ee
Khasanov Yulai <khasanov_iu@ufanet.ru>
2022-08-14 19:16:49 +0500

    Выделил токен в отдельный файл

--- bot.py
+++ bot.py
@@ -1,19 +1,18 @@
+import logging
 import sqlite3
 from datetime import datetime, timezone, timedelta
-import logging
 
 import telebot
 from apscheduler.executors.pool import ThreadPoolExecutor
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 from apscheduler.schedulers.background import BackgroundScheduler
-from telebot import types
 from pytz import utc
+from telebot import types
 
-TOKEN = '5542237999:AAG1Oy8z08aEHyieytR2cL2eu22fpZ8mCag'
+from .settings import TOKEN, ADMIN_ID
 
 DB = None
 BOT = telebot.TeleBot(TOKEN)
-ADMIN_ID = [927060137, 304440895]
 
 
 def enter_date_step(message):
